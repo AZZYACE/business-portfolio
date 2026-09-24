65fc1934da91817ada353c10291ec953a3d7dfdf
aizecabansag-admin <aizecabansag120805@gmail.com>
2026-09-24 18:51:31 +0800

    Initial React Vite project setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 import Header from './components/Header';
 import HomeSection from './components/Home/HomeSection';
 import ProductsSection from './components/Products/ProductsSection';
